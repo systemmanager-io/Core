914db6e89ce3f475ba1a58040c178673628dff3b
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2019-05-02 00:10:33 +0200

    Start of SystemManager frontend!

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+    </head>
+    <body>
+        <noscript>You need JavaScript to run this application</noscript>
+        <div id="systemmanagerapp"></div>
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
