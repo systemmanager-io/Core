57530ea981ff542ec3ffe23712ddc790d675abd7
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2019-10-01 13:07:17 +0200

    Update pingServers.ts

--- worker/src/Jobs/pingServers.ts
+++ worker/src/Jobs/pingServers.ts
@@ -1,6 +1,6 @@
 import {workerDebug} from "../Lib/debugs";
 
-export default function (job, jobDone) {
+export default function (job: any, jobDone: any) {
     workerDebug("Pinging Server on" + job.process.pid);
     console.log("Pinging Server");
     jobDone();
