7347935d70a1eb00e9b214b60bfb20774062824a
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2019-10-14 10:44:14 +0200

    Throw error on no password match

--- core/src/Lib/Errors/GraphQL/ErrorTypes.ts
+++ core/src/Lib/Errors/GraphQL/ErrorTypes.ts
@@ -2,6 +2,15 @@ const errorType: any = {
     NOTFOUND: {
         message: `Document has not been found in the database`,
         statusCode: 404
+    },
+    INTERNALERROR: {
+        message: `Internal Server Error`,
+        statusCode: 500
+    },
+
+
+    NOPASSWORDMATCH: {
+        message: `Passwords no not match`,
     }
 }
 export default errorType;
