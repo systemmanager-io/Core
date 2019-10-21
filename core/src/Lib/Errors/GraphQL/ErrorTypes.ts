1772aa0f8c0197ce7a97c3d7b5f57ab8beff3442
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2019-10-21 15:36:27 +0200

    Added more errors to the errorlist for the user groups and permissions

--- core/src/Lib/Errors/GraphQL/ErrorTypes.ts
+++ core/src/Lib/Errors/GraphQL/ErrorTypes.ts
@@ -1,19 +1,24 @@
 const errorType: any = {
     NOTFOUND: {
-        message: `Document has not been found in the database`,
+        message: `Document has not been found in the database.`,
         statusCode: 404
     },
     INTERNALERROR: {
-        message: `Internal Server Error`,
+        message: `Internal Server Error.`,
         statusCode: 500
     },
 
 
     NOPASSWORDMATCH: {
-        message: `Passwords do not match`,
+        message: `Passwords do not match.`,
     },
     USERALREADYEXISTS: {
-        message: `User already exists`
+        message: `User already exists.`
+    },
+
+    NOPERMISSION: {
+        message: `You do not have permission to execute this action.`,
+        statusCode: 403
     }
 }
 export default errorType;
