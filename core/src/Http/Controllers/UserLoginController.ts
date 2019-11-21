65f471ba436200e7015f8972cc321a9961bbe027
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2019-11-21 15:28:10 +0100

    Using argon2 as password hashing method. Will soon start building on the middleware to check every request that is being made to the server

--- core/src/Http/Controllers/UserLoginController.ts
+++ core/src/Http/Controllers/UserLoginController.ts
@@ -4,16 +4,9 @@ import {httpDebug} from "../../Lib/debug";
 import {ArrayCursor} from "arangojs/lib/async/cursor";
 import {arangodb} from "../../connectors";
 import {aql, AqlQuery} from "arangojs/lib/cjs/aql-query";
+import argon from "argon2";
 
-// export default function UserLoginController() {
-//
-// let token = jwt.sign({ foo: 'bar' }, 'shhhhh');
-//
-// console.log(token);
-//
-// return;
-// }
-
+const incorrectInfo = {status: "Incorrect login information"};
 export default class UserLoginController {
 
     public static async login(req: Request, res: Response, next: NextFunction) {
@@ -24,13 +17,15 @@ export default class UserLoginController {
         const password: any = req.headers.password;
 
         if (email == undefined || password == undefined) {
-            res.send({error: "Email/Password not set"});
+            res.status(401);
+            res.send(incorrectInfo);
             return
         }
 
         const query: AqlQuery = aql`
         FOR u in users
             FILTER u.email == @email
+            LIMIT 1
         RETURN u
         `;
 
@@ -39,18 +34,22 @@ export default class UserLoginController {
         };
 
         const queryResult: ArrayCursor = await arangodb.query(query);
-        const userAccount = await queryResult.all();
 
-        if(!Array.isArray(userAccount) || !userAccount.length) {
-            console.log("User exists")
-            console.log(userAccount);
+        // This seems little off to do all(). But i have limited the query to show/retrieve the first user only
+        const userAccount: any = await queryResult.all();
+
+        if (Array.isArray(userAccount) && userAccount.length != 0) {
+            // The const below this thing feels SO broken.
+            const user = userAccount[0];
+            if (await argon.verify(user.password, password + user.salt)) {
+                res.send({status: "YEAAAAAAAAAAA WHOOOO PASSWORD CORRECT!"})
+            } else {
+                res.status(401);
+                res.send(incorrectInfo)
+            }
+        } else {
+            res.status(401);
+            res.send(incorrectInfo);
         }
-        // console.log(all);
-        // console.log(email, password)
-
-
-        res.send({test: "test"});
-
     }
-
 }
